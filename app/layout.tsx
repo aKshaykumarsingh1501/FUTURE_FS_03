40760db4bc4a21c78cd14a1ee4bc1a2b55355fce
aKshaykumarsingh1501 <akshaykrsingh.jsr@gmail.com>
2026-01-10 00:19:09 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,17 +15,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+  <body className="bg-black text-white min-h-screen overflow-x-hidden">
+    <CartProvider>
+      <Navbar />
 
-      <body className="bg-black text-white">
-        <CartProvider>
-          <Navbar />
+      <div className="pt-16 flex flex-col min-h-screen">
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </CartProvider>
+  </body>
+</html>
 
-          <div className="pt-16 flex flex-col min-h-screen">
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </CartProvider>
-      </body>
-    </html>
   );
 }
